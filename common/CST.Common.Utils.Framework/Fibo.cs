68313448023dd459743fe39b00067b113fc46041
Csernák Tamás <csernak.tamas91@gamil.com>
2019-11-24 20:48:03 +0100

    added demo lib targeting dotnet standard

---
+++ common/CST.Common.Utils.Framework/Fibo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CST.Common.Utils.Framework
+{
+    public class Fibo
+    {
+        private readonly FiboCache fiboCache;
+
+        public Fibo(FiboCache fiboCache)
+        {
+            this.fiboCache = fiboCache;
+        }
+
+        public int GetNthNumber(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            var (found, val) = fiboCache.TryGetValue(n);
+            if (found)
+            {
+                return val;
+            }
+
+            val = GetNthNumber(n - 1) + GetNthNumber(n - 2);
+            fiboCache.Store(n, val);
+
+            return val;
+        }
+    }
+}
---
+++ common/CST.Common.Utils.Framework/FiboCache.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace CST.Common.Utils.Framework
+{
+    public class FiboCache
+    {
+        private readonly Dictionary<int, int> values;
+        public FiboCache()
+        {
+            values = new Dictionary<int, int>();
+        }
+
+        public (bool found, int val) TryGetValue(int n)
+        {
+            if (values.ContainsKey(n))
+            {
+                return (true, values[n]);
+            }
+
+            return (false, default(int));
+        }
+
+        public void Store(int n, int val)
+        {
+            values.Add(n, val);
+        }
+    }
+}
