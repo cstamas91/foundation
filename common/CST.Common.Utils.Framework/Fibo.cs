eb1e6bd591a20cd0fc5d1187a21d0f61b7943fac
Csernák Tamás <csernak.tamas91@gamil.com>
2019-11-24 21:29:25 +0100

    asd

--- common/CST.Common.Utils.Framework/Fibo.cs
+++ common/CST.Common.Utils.Framework/Fibo.cs
@@ -4,29 +4,13 @@ namespace CST.Common.Utils.Framework
 {
     public class Fibo
     {
-        private readonly FiboCache fiboCache;
-
-        public Fibo(FiboCache fiboCache)
-        {
-            this.fiboCache = fiboCache;
-        }
-
         public int GetNthNumber(int n)
         {
             if (n == 1 || n == 2)
             {
                 return 1;
             }
-
-            var (found, val) = fiboCache.TryGetValue(n);
-            if (found)
-            {
-                return val;
-            }
-
-            val = GetNthNumber(n - 1) + GetNthNumber(n - 2);
-            fiboCache.Store(n, val);
-
+            var val = GetNthNumber(n - 1) + GetNthNumber(n - 2);
             return val;
         }
     }
