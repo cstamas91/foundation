e90ec65efc2844ff6e1b7f8a04bb361cc50b4b51
Csernák Tamás <csernak.tamas91@gamil.com>
2020-02-14 20:40:03 +0100

    Added Selectable class
    This can serve as a basic DTO for types that could become
    data sources for 'select' UI controls.

---
+++ common/common.utils/ViewModel/Selectable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CST.Common.Utils.ViewModel
+{
+    public class Selectable<TKey>
+        where TKey : struct, IEquatable<TKey>
+    {
+        public TKey Id { get; set; }
+        public string Name { get; set; }
+    }
+}
