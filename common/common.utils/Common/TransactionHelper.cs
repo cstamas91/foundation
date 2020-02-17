2a8de403a84329404a33ab4283a4c2d068e0e656
Csernák Tamás <csernak.tamas91@gamil.com>
2020-02-17 19:26:43 +0100

    TransactionHelper refactor Changed this helper to use a simple Action parameter instead of Func

---
+++ common/common.utils/Common/TransactionHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Transactions;
+using Microsoft.Extensions.Logging;
+
+namespace CST.Common.Utils.Common
+{
+    public static class TransactionHelper
+    {
+        public static void WithTransactionScope(Action f, ILogger logger = null)
+        {
+            try
+            {
+                using var tran = new TransactionScope();
+                f();
+                tran.Complete();
+            }
+            catch (Exception exception)
+            {
+                logger?.LogError(exception, string.Empty);
+                throw;
+            }
+        }
+    }
+}
