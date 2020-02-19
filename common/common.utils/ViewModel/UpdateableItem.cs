301561f1723e5d3d9c125d4b61c7cadfefb91823
Csernák Tamás <csernak.tamas91@gamil.com>
2020-02-19 21:34:22 +0100

    Added implementation for ChildCollection

--- common/common.utils/ViewModel/UpdateableItem.cs
+++ common/common.utils/ViewModel/UpdateableItem.cs
@@ -1,23 +1,15 @@
 ﻿namespace CST.Common.Utils.ViewModel
 {
-    public enum StateFlag
-    {
-        Unchanged,
-        Added,
-        Deleted,
-        Modified
-    }
-    
     public class UpdateableItem<T>
     {
-        public StateFlag State { get; set; }
+        public UpdateableItemState UpdateableItemState { get; set; }
         public T Item { get; set; }
 
         public static UpdateableItem<T> Unchanged(T item)
         {
             return new UpdateableItem<T>
             {
-                State = StateFlag.Unchanged,
+                UpdateableItemState = UpdateableItemState.Unchanged,
                 Item = item
             };
         }
