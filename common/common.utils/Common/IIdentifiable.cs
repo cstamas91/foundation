24411b11efeca2e781286efbad2289b0340c0ce1
Csernák Tamás <csernak.tamas91@gamil.com>
2020-02-24 18:08:24 +0100

    Created IIdentifiable interface for classes that have some kind of Id field

---
+++ common/common.utils/Common/IIdentifiable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CST.Common.Utils.Common
+{
+    public interface IIdentifiable<TKey>
+        where TKey: struct, IEquatable<TKey>
+    {
+        TKey Id { get; set; }
+    }
+}
