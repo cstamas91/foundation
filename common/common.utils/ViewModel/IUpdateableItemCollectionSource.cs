646be0d9689f11589335cc7ed6c1b49da4cd5f7c
Csernák Tamás <csernak.tamas91@gamil.com>
2020-02-24 21:57:08 +0100

    Added interfaces for generic manipulation of entities owning a childcollection

---
+++ common/common.utils/ViewModel/IUpdateableItemCollectionSource.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CST.Common.Utils.ViewModel
+{
+    public interface IUpdateableItemCollectionSource<TItem>
+    {
+        IEnumerable<UpdateableItem<TItem>> GetSourceCollection();
+    }
+
+    public interface IUpdateableItemCollectionDestination<TItem>
+    {
+        ICollection<TItem> GetDestinationCollection();
+    }
+}
