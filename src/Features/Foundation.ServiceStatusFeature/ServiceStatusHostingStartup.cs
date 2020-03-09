19619c575d63787285a65289604142a4a8a639a0
Csernák Tamás <csernak.tamas91@gamil.com>
2020-03-09 22:20:42 +0100

    added .editorconfig, HostingStartup assembly attribute

--- src/Features/Foundation.ServiceStatusFeature/ServiceStatusHostingStartup.cs
+++ src/Features/Foundation.ServiceStatusFeature/ServiceStatusHostingStartup.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Foundation.ServiceStatusFeature;
+using Microsoft.AspNetCore.Hosting;
 
+[assembly: HostingStartup(typeof(ServiceStatusHostingStartup))]
 namespace Foundation.ServiceStatusFeature
 {
-    
     public class ServiceStatusHostingStartup : IHostingStartup
     {
         public void Configure(IWebHostBuilder builder)
--- src/Foundation.Core/ViewModel/Selectable.cs
+++ src/Foundation.Core/ViewModel/Selectable.cs
@@ -13,11 +13,7 @@ namespace Foundation.Core.ViewModel
     public class Selectable<TKey>
         where TKey : struct, IEquatable<TKey>
     {
-        [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
-        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
         public TKey Id { get; set; }
-        [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
-        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
         public string Name { get; set; }
 
         public static Selectable<TKey> Create(ISelectableSource<TKey> source)
